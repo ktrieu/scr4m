ae527b3348c260f646aa36c45dbdeba86278f258
Kevin Trieu <kevin@enzuzo.com>
2024-05-18 11:05:24 -0400

    Add Fastify setup

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+    logger: true
+})
+
+fastify.get("/", (request, reply) => {
+    reply.send("yeah that's great")
+});
+
+fastify.listen({ port: 6000 })
