c0e8e2c15c57ecd9ca5b08cd4358a4b7834d6f5c
Kevin Trieu <kevin@enzuzo.com>
2024-05-19 02:06:37 -0400

    Get front-end dev setup working

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  const port = +env.VITE_PORT ?? 3000;
+
+  return {
+    plugins: [react()],
+    server: {
+      port,
+    }
+  }
 })
