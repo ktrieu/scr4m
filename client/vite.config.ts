1d9dedae64ffaa7308706b90ad20b78425ca5f60
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-05-25 16:44:46 -0400

    Fix warning in Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
-  const port = +env.VITE_PORT ?? 3000;
+  const port = parseInt(env.VITE_PORT)
 
   return {
     plugins: [react()],
