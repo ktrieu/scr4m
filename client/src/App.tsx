29993c19116ce325b27ed48e753257f4b1ff5ba3
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-06-06 01:54:13 -0400

    Remove test code for common lib

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
 import { GoogleLogin } from "./components/GoogleLogin"
 
-import { RegisterBodySchema } from "@scr4m/common"
-
 
 function App() {
-  console.log(RegisterBodySchema.parse({ token: '' }))
   return (
     <>
       <GoogleLogin />
