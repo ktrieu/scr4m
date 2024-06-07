a43ecf05a4baad2b0c9b885cfa97accbfcf2fb40
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-06-07 03:21:13 -0400

    Add a nice comforting background

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,10 @@ import { GoogleLogin } from "./components/GoogleLogin"
 
 function App() {
   return (
-    <>
+    <div className="bg-secondary h-screen">
       <p className="text-xl">HELLO</p>
       <GoogleLogin />
-    </>
+    </div>
   )
 }
 
