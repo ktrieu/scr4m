63ba24aaf5b9796d46da0d8d03dbcddcf7c82e53
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-06-09 01:51:42 -0400

    Add register route

--- client/src/components/layout/AuthLayout.tsx
+++ client/src/components/layout/AuthLayout.tsx
@@ -7,7 +7,7 @@ type AuthLayoutProps = {
 
 export const AuthLayout = (props: AuthLayoutProps) => {
     return <div className="mx-auto flex flex-col justify-center items-center w-1/3 min-w-36 h-full">
-        <div className="flex flex-col h-fit items-center border-primary border-2 rounded-sm p-16 bg-white drop-shadow-lg">
+        <div className="flex flex-col h-fit items-center border-primary border-2 rounded-sm p-16 bg-white drop-shadow-lg w-1/2">
             <Logo />
             {props.children}
         </div>
--- client/src/routeTree.gen.ts
+++ client/src/routeTree.gen.ts
@@ -13,6 +13,7 @@ import { createFileRoute } from '@tanstack/react-router'
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as RegisterImport } from './routes/register'
 import { Route as LoginImport } from './routes/login'
 
 // Create Virtual Routes
@@ -20,6 +21,11 @@ import { Route as LoginImport } from './routes/login'
 const IndexLazyImport = createFileRoute('/')()
 
 // Create/Update Routes
+
+const RegisterRoute = RegisterImport.update({
+  path: '/register',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const LoginRoute = LoginImport.update({
   path: '/login',
@@ -49,12 +55,23 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof LoginImport
       parentRoute: typeof rootRoute
     }
+    '/register': {
+      id: '/register'
+      path: '/register'
+      fullPath: '/register'
+      preLoaderRoute: typeof RegisterImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren({ IndexLazyRoute, LoginRoute })
+export const routeTree = rootRoute.addChildren({
+  IndexLazyRoute,
+  LoginRoute,
+  RegisterRoute,
+})
 
 /* prettier-ignore-end */
 
@@ -65,7 +82,8 @@ export const routeTree = rootRoute.addChildren({ IndexLazyRoute, LoginRoute })
       "filePath": "__root.tsx",
       "children": [
         "/",
-        "/login"
+        "/login",
+        "/register"
       ]
     },
     "/": {
@@ -73,6 +91,9 @@ export const routeTree = rootRoute.addChildren({ IndexLazyRoute, LoginRoute })
     },
     "/login": {
       "filePath": "login.tsx"
+    },
+    "/register": {
+      "filePath": "register.tsx"
     }
   }
 }
