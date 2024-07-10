d3197dd39fa97a9c6536ff5837ec43a6b6a85a4e
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-07-10 01:10:27 -0400

    Finish implementing the session store

--- server/src/auth/session.ts
+++ server/src/auth/session.ts
@@ -1,7 +1,13 @@
 import type { FastifySessionOptions, SessionStore } from "@fastify/session";
-import { ENV_CONFIG } from "../env.js";
 import type { Kysely } from "kysely";
+import { ENV_CONFIG } from "../env.js";
 import type Database from "../schemas/Database.js";
+import type { Session } from "fastify";
+import {
+	createSession,
+	deleteSession,
+	getSessionById,
+} from "../db/session/index.js";
 
 declare module "fastify" {
 	interface Session {
@@ -9,12 +15,69 @@ declare module "fastify" {
 	}
 }
 
+// Seven days, in ms.
+const SESSION_EXPIRY_MS = 1000 * 60 * 60 * 24 * 7;
+
+const asyncGetSession = async (
+	db: Kysely<Database>,
+	sessionId: string,
+): Promise<Session | undefined> => {
+	const session = await getSessionById(db, sessionId, new Date());
+
+	if (session === undefined) {
+		return undefined;
+	}
+
+	return {
+		user_id: session.user_id,
+		// biome-ignore lint/suspicious/noExplicitAny: We can assume that the type of the cookie JSON is correct since we only insert a valid session.
+		cookie: <any>session.cookie,
+	};
+};
+
+const asyncSetSession = async (
+	db: Kysely<Database>,
+	sessionId: string,
+	session: Session,
+) => {
+	const expiry = Date.now() + SESSION_EXPIRY_MS;
+	await createSession(db, sessionId, new Date(expiry), session);
+};
+
+const asyncDeleteSession = async (db: Kysely<Database>, sessionId: string) => {
+	await deleteSession(db, sessionId);
+};
+
 const createSessionStore = (db: Kysely<Database>): SessionStore => {
 	// Bunch of no-op stuff for now.
 	return {
-		get: (sessionId, callback) => {},
-		set: (sessionId, session, callback) => {},
-		destroy: (sessionId, callback) => {},
+		get: (sessionId, callback) => {
+			asyncGetSession(db, sessionId)
+				.then((session) => {
+					callback(null, session);
+				})
+				.catch((err) => {
+					callback(err);
+				});
+		},
+		set: (sessionId, session, callback) => {
+			asyncSetSession(db, sessionId, session)
+				.then(() => {
+					callback(null);
+				})
+				.catch((err) => {
+					callback(err);
+				});
+		},
+		destroy: (sessionId, callback) => {
+			asyncDeleteSession(db, sessionId)
+				.then(() => {
+					callback(null);
+				})
+				.catch((err) => {
+					callback(err);
+				});
+		},
 	};
 };
 
---
+++ server/src/db/session/index.ts
@@ -0,0 +1,41 @@
+import type { Session } from "fastify";
+import type { SessionsId } from "../../schemas/public/Sessions.js";
+import type { UsersId } from "../../schemas/public/Users.js";
+import type { DbInstance } from "../index.js";
+
+export const getSessionById = async (
+	db: DbInstance,
+	id: string,
+	expiresBefore: Date,
+) => {
+	return db
+		.selectFrom("sessions")
+		.selectAll()
+		.where("id", "=", <SessionsId>id)
+		.where("expiry", "<", expiresBefore)
+		.executeTakeFirst();
+};
+
+export const createSession = async (
+	db: DbInstance,
+	id: string,
+	expiry: Date,
+	session: Session,
+) => {
+	return db
+		.insertInto("sessions")
+		.values({
+			id: <SessionsId>id,
+			expiry,
+			user_id: <UsersId>session.user_id,
+			cookie: session.cookie,
+		})
+		.execute();
+};
+
+export const deleteSession = async (db: DbInstance, id: string) => {
+	return db
+		.deleteFrom("sessions")
+		.where("id", "=", <SessionsId>id)
+		.execute();
+};
