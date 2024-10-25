e06310467a7b75b8b37fecf5029cf6910bbb9330
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-10-25 11:48:24 -0400

    Rename done to todid in scrum_entry_type
    Can't read my own scrum sheet, apparently.

---
+++ server/src/migrations/2024-10-25-1145_scrum_type_fix.ts
@@ -0,0 +1,13 @@
+import { sql, type Kysely } from "kysely";
+
+export async function up(db: Kysely<any>) {
+	await sql`alter type scrum_entry_type rename value 'done' to 'todid'`.execute(
+		db,
+	);
+}
+
+export async function down(db: Kysely<any>) {
+	await sql`alter type scrum_entry_type rename value 'todid' to 'done'`.execute(
+		db,
+	);
+}
