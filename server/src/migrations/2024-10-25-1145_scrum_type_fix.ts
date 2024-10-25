f75f1f3b8102637e07541c762796b1b793adb675
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-10-25 11:49:32 -0400

    Fix imports

--- server/src/migrations/2024-10-25-1145_scrum_type_fix.ts
+++ server/src/migrations/2024-10-25-1145_scrum_type_fix.ts
@@ -1,4 +1,4 @@
-import { sql, type Kysely } from "kysely";
+import { type Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<any>) {
 	await sql`alter type scrum_entry_type rename value 'done' to 'todid'`.execute(
