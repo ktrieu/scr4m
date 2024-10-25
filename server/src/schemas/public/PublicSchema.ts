4b423d0e1246fc121ed5dff7b82c096533a055f7
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-10-25 13:10:52 -0400

    Regenerate schema

--- server/src/schemas/public/PublicSchema.ts
+++ server/src/schemas/public/PublicSchema.ts
@@ -1,6 +1,9 @@
 import type CompaniesTable from "./Companies.js";
 import type KyselyMigrationTable from "./KyselyMigration.js";
 import type KyselyMigrationLockTable from "./KyselyMigrationLock.js";
+import type ScrumEntriesTable from "./ScrumEntries.js";
+import type ScrumMembersTable from "./ScrumMembers.js";
+import type ScrumsTable from "./Scrums.js";
 import type SessionsTable from "./Sessions.js";
 import type UsersTable from "./Users.js";
 
@@ -14,4 +17,10 @@ export default interface PublicSchema {
 	users: UsersTable;
 
 	sessions: SessionsTable;
+
+	scrums: ScrumsTable;
+
+	scrum_members: ScrumMembersTable;
+
+	scrum_entries: ScrumEntriesTable;
 }
