21caafd0ac13eccad545734d46944a917bd28627
Kevin Trieu <kevin@enzuzo.com>
2024-11-03 22:37:02 -0500

    Return scrum presence tracking in API

--- server/src/db/scrum/index.ts
+++ server/src/db/scrum/index.ts
@@ -1,4 +1,5 @@
 import type { CompaniesId } from "../../schemas/public/Companies.js";
+import { ScrumAttendances } from "../../schemas/public/ScrumAttendances.js";
 import type { ScrumEntries } from "../../schemas/public/ScrumEntries.js";
 import type { ScrumMembers } from "../../schemas/public/ScrumMembers.js";
 import type { Scrums, ScrumsId } from "../../schemas/public/Scrums.js";
@@ -52,6 +53,13 @@ export const getOrderedEntriesForScrum = async (
 		.orderBy("order asc")
 		.execute();
 };
+
+export const getAttendanceForScrum = async (
+	db: DbInstance,
+	scrumId: ScrumsId
+): Promise<Array<ScrumAttendances>> => {
+	return db.selectFrom("scrum_attendances").selectAll().where("scrum_id", "=", scrumId).execute();
+}
 
 export const getMembersForCompany = async (
 	db: DbInstance,
