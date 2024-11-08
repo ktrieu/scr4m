cd448a2c3631be5854da5b512bba35a7b434f709
Kevin Trieu <kevin.trieu5813@gmail.com>
2024-11-08 13:28:46 -0500

    Automatically resize our text area component

--- client/src/components/TextArea.tsx
+++ client/src/components/TextArea.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 export type TextAreaSize = "large" | "default";
 
 type TextAreaProps = {
@@ -21,9 +23,24 @@ export const TextArea = (props: TextAreaProps) => {
 
 	const fontSizeClass = getFontSizeClass(size);
 
+	const ref = useRef<HTMLTextAreaElement | null>(null);
+
+	// Clever trick to autosize the textarea from here: https://medium.com/@oherterich/creating-a-textarea-with-dynamic-height-using-react-and-typescript-5ed2d78d9848
+	// biome-ignore lint/correctness/useExhaustiveDependencies: We need to recalc height when the value changes, even if we don't use it.
+	useEffect(() => {
+		const { current: textArea } = ref;
+		if (textArea) {
+			textArea.style.height = "0px";
+			const scrollHeight = textArea.scrollHeight;
+
+			textArea.style.height = `${scrollHeight}px`;
+		}
+	}, [ref, value]);
+
 	return (
 		<textarea
 			className={`bg-secondary border-none w-full resize-none focus:ring-0 focus:bg-white ${fontSizeClass}`}
+			ref={ref}
 			value={value}
 			onChange={(e) => {
 				onChange(e.currentTarget.value);
